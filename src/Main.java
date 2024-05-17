bb6eaeee668b53a5f0f26cb317183c033e06f37b
azoozin <mario.github@protonmail.com>
2024-05-17 17:56:59 -0300

    add: base classes and functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("hello world");
+        Cliente client = new Cliente();
+        client.setNome("ClientOne");
+
+        Conta cc = new ContaCorrente(client);
+        Conta poupanca = new ContaPoupanca(client);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
     }
 }
