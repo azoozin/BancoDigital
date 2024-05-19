577d6e89c9b8eec05f89c5cbd10b97667e25993f
azoozin <mario.github@protonmail.com>
2024-05-19 00:13:36 -0300

    small changes

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
         Banco banco = new Banco();
 
-        Cliente client = new Cliente("ClientOne");
-        Conta cc = new ContaCorrente(client);
-        banco.adicionarContaCorrente(cc);
-        Conta poupanca = new ContaPoupanca(client);
-        banco.adicionarContaPoupanca(poupanca);
-        Emprestimo emp = new Emprestimo(client);
+        // Cliente client = new Cliente("ClientOne");
+        // Conta cc = new ContaCorrente(client);
+        // banco.adicionarContaCorrente(cc);
+        // Conta poupanca = new ContaPoupanca(client);
+        // banco.adicionarContaPoupanca(poupanca);
+        // Emprestimo emp = new Emprestimo(client);
 
-        /*
-         * TODO:
-         * 
-         * Adicionar class UI
-         * Fazer listas ContaCorrente e Poupanca de class Banco em Sets
-         * Verificar se conta ja existe e negar ciracao de uma nova no nome do titular
-         * 
-         */
     }
 }
